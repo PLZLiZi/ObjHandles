476ff4c26df7edeb8f1e02efe55971341990ed68
PLZLiZi <plz_lizi@outlook.com>
2025-07-26 10:40:37 +0800

    UPDATE

--- prebuild.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-chcp 65001>nul
-copy ".\src\main\c\jvmtiinst\Build\jvmtiinst-x86.dll" "D:\PLZLiZi\Java\ObjHandles\app\src\main\resources\plz\lizi\objhandles" /Y>nul
-copy ".\src\main\c\jvmtiinst\Build\jvmtiinst-x64.dll" "D:\PLZLiZi\Java\ObjHandles\app\src\main\resources\plz\lizi\objhandles" /Y>nul
