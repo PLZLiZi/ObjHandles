d27305f8b52a6930af31afe149df2f6aaaac940a
PLZLiZi <plz_lizi@outlook.com>
2025-08-23 11:35:29 +0800

    update

--- lib/src/main/java/plz/lizi/objhandles/Main.java
+++ lib/src/main/java/plz/lizi/objhandles/Main.java
@@ -4,10 +4,5 @@ import com.sun.tools.attach.VirtualMachine;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println(new FieldHandle("sun.tools.attach.HotSpotVirtualMachine.ALLOW_ATTACH_SELF").getObject());
-		new FieldHandle("sun.tools.attach.HotSpotVirtualMachine.ALLOW_ATTACH_SELF").set(true);
-		System.out.println(new FieldHandle("sun.tools.attach.HotSpotVirtualMachine.ALLOW_ATTACH_SELF").getObject());
-		new FieldHandle("sun.tools.attach.HotSpotVirtualMachine.ALLOW_ATTACH_SELF").set(false);
-		System.out.println(new FieldHandle("sun.tools.attach.HotSpotVirtualMachine.ALLOW_ATTACH_SELF").getObject());
 	}
 }
